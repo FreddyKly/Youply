bf7147a131a12f78d72ae5ad57f6e905b8fc8e3d
Freddy <Frederik.Kliemt@web.de>
2020-06-03 15:25:55 +0200

    Audio Files are downloaded but wrong format

--- downloader.py
+++ downloader.py
@@ -1,16 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
+from pytube import YouTube
+import youtube_dl
 
 class Downloader:
     def collect_links(self, url):
         html = requests.get(url)
         soup = BeautifulSoup(html.text, 'html.parser')
         links = soup.find_all('a', class_="pl-video-title-link yt-uix-tile-link yt-uix-sessionlink spf-link")
-        titles = []
+        self.titles = []
+        self.video_links = []
         for link in links:
-            titles.append(link.text.strip())
+            self.titles.append(link.text.strip())
             href_start = str(link).find('href=')
             href_end = str(link).find(';list=')
             link_fract = str(link)[href_start + 6:href_end]
-            valid_link = 'youtube.com' + link_fract
-            print(valid_link)
+            valid_link = 'https://www.youtube.com' + link_fract
+            self.video_links.append(valid_link)
+            print('Collected all Links')
+        return self.video_links
+
+    def process_links_mp4(self, links):
+        pass
+
+    def process_links_mp3(self, links):
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }      
+        print('Start downloads') 
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(links)
+            print('Downloaded: ', self.titles)
--- get_started.py
+++ get_started.py
@@ -1,8 +1,12 @@
-from pytube import YouTube
 from downloader import Downloader
 import sys
 
 def run():
     url = sys.argv[1]
     downloader = Downloader()
-    downloader.collect_links(url)
+    links = downloader.collect_links(url)
+    file_ext = 'mp3'
+    if file_ext == 'mp3':
+        downloader.process_links_mp3(links)
+    elif file_ext == 'mp4':
+        downloader.process_links_mp4(links)
