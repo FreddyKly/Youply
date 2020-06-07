9585373d91cc98a9ac9eacba7e485df14df0fdf4
Freddy <Frederik.Kliemt@web.de>
2020-06-07 23:56:14 +0200

    venv swapped for pipenv and kivy is now itegrated

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
+kivy.require("1.11.1")
+
+class StartPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.add_widget(Label(text='Speicherort: '))
 
 class MyApp(App):
-
     def build(self):
         return Label(text='Hello world')
